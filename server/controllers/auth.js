ccb095bce49082734b8afe03f47f51b7b13d993c
sourabhdeshmukh <sourabh.deshmukh.988@gmail.com>
2023-08-20 19:20:05 -0500

    Adding backed config for user registration.

---
+++ server/controllers/auth.js
@@ -0,0 +1,42 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../modules/User.js");
+
+// New User Registration
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        });
+
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
