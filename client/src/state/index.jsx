11970730433693588d223dfe6ef145bbba20ead5
sourabhdeshmukh <sourabh.deshmukh.988@gmail.com>
2023-08-23 00:32:49 -0500

    Addinng user actions states to project.

--- client/src/state/index.jsx
+++ client/src/state/index.jsx
@@ -30,7 +30,17 @@ export const authSlice = createSlice({
             }
         },
         setPosts: (state, action) => {
-            
-        }
-    }
-})
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
+        },
+    },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
